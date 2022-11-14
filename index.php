7066a958758744be3718baf70bf45855bda1a98c
Josip Harci <harcijosip99@gmail.com>
2022-11-14 22:57:33 +0100

    Update

--- index.php
+++ index.php
@@ -11,13 +11,10 @@
 </head>
 <body>
  <header>
-  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <nav class="navbar">
   <a class="navbar-brand" href="#"><img src="assets/images/logo.svg" alt="logo"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+  <div id="navbarNav">
+    <ul class="nav justify-content-end">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home</a>
       </li>
