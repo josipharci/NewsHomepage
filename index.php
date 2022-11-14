fd0c4463a071f5f7f49bcd888cc77748479271c2
Josip Harci <harcijosip99@gmail.com>
2022-11-14 21:57:23 +0100

    Fix

--- index.php
+++ index.php
@@ -3,12 +3,28 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="assest/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="assest/css/style.css">
-	<link rel="stylesheet" type="text/css" href="assest/css/media-query.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/media-query.css">
 	<title>NewsHomepage</title>
 </head>
 <body>
+ <header>
+ <nav class="navbar navbar-default" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>    
+  </div>
+  <div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Right</a></li>
+    </ul>
+  </div>
+ </nav>
+</header>
 
 </body>
     <script src="assest/js/bootstrap.bundle.min.js"></script>
