c8d330fd56f5aee253e8d7930e9b3b46912a5164
Josip Harci <harcijosip99@gmail.com>
2022-11-20 16:33:17 +0100

    Update
    Add Navbar

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector(".body"),
- 	  nav  = document.querySelector(".nav"),
-	  sliderOpen = document.querySelector(".sliderbarOpen"),
-      sliderClose = document.querySelector(".sliderbarClose");
-
-sliderOpen.addEventListener("click" , () =>{
- 	nav.classList.add("active");
-})
